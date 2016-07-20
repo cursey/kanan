03b16a6450816de99e4af64702264eb189baa0dc
QewQew <QewQew@users.noreply.github.com>
2016-07-20 15:35:33 -0700

    Added OneClickRevive Patch

---
+++ scripts/OneClickRevive.js
@@ -0,0 +1,16 @@
+// One Click Revive
+
+var pattern = scan('client.exe', '39 ?? ?? 0F 86 ?? ?? ?? ?? 8B ?? ?? 8B 11');
+
+if (debug)
+{
+	send(pattern);
+}
+
+if (pattern == NULL)
+	send('Failed to apply patch.');
+else
+{
+	patch(pattern.add(3), 0x90);
+	patch(pattern.add(4), 0xE9);
+}
