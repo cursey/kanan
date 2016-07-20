ec938967800a97bfe72987e4f6da347149cf986e
QewQew <QewQew@users.noreply.github.com>
2016-07-20 14:56:12 -0700

    Update BitmapFont.js
    Check if scans succeed before applying patch.

--- scripts/BitmapFont.js
+++ scripts/BitmapFont.js
@@ -2,18 +2,19 @@
 // All credit to Blade3575
 
 var bm1 = scan('client.exe', '80 BF 88 00 00 00 00 57 74 0D');
-
-if (debug) {
-    send(bm1);
-}
-
-patch(bm1.add(8), 0x90);
-patch(bm1.add(9), 0x90);
-
 var bm2 = scan('client.exe', '80 BE 88 00 00 00 00 74 ?? B9 ?? ?? ?? ?? E8 ?? ?? ?? ?? 83 F8 01 75');
 
 if (debug) {
+    send(bm1);
     send(bm2);
 }
 
-patch(bm2.add(22), 0xEB);
+if (bm1 == NULL || bm2 == NULL)
+	send('Failed to apply patch.');
+else
+{
+	patch(bm1.add(8), 0x90);
+	patch(bm1.add(9), 0x90);
+
+	patch(bm2.add(22), 0xEB);
+}
