c3598360d70950d79f0f5dfcbf4b46edccdf9627
Coziest <paraparasomnia@gmail.com>
2016-07-23 21:36:13 -0700

    Add files via upload

---
+++ scripts/AlwaysTransCollectMode.js
@@ -0,0 +1,15 @@
+// Transformation Mastery Collect Mode Always Enabled (Rydian)
+
+var pattern = scan('38 5F 78 0F 84 C9 02 00 00 8B CE');
+
+if (debug)
+{
+	send(pattern);
+}
+
+if (pattern == NULL)
+	send('Failed to apply patch.');
+else
+{
+	patch(pattern.add(4), 0x85);
+}
---
+++ scripts/FighterNoForce.js
@@ -0,0 +1,11 @@
+// No Persistent Fighter Chain Popup (Rydian)
+
+var pattern = scan('83 79 44 01 75 09 6A 00 6A 00 E8');
+
+if (pattern == NULL)
+	send('Failed to apply patch.');
+else
+{
+	patch(pattern.add(4), 0xEB);
+	patch(pattern.add(5), 0x09);
+}
