a01c04a783282fa0b7b89b9caf601d07a030574d
QewQew <QewQew@users.noreply.github.com>
2016-07-24 00:09:59 -0700

    Update FighterNoForce.js
    Removed unnecessary byte patch.

--- scripts/FighterNoForce.js
+++ scripts/FighterNoForce.js
@@ -5,7 +5,4 @@ var pattern = scan('83 79 44 01 75 09 6A 00 6A 00 E8');
 if (pattern == NULL)
 	send('Failed to apply patch.');
 else
-{
 	patch(pattern.add(4), 0xEB);
-	patch(pattern.add(5), 0x09);
-}
