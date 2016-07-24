d5201d9b9b2932708baf3dd875890d4c74eeb165
QewQew <QewQew@users.noreply.github.com>
2016-07-24 00:25:19 -0700

    Update FreeIndoorCamera.js

--- scripts/FreeIndoorCamera.js
+++ scripts/FreeIndoorCamera.js
@@ -5,7 +5,4 @@ var pattern = scan('57 8b 7d 08 0f 84 22 02 00 00');
 if (pattern == NULL)
 	send('Failed to apply patch.');
 else
-{
-	patch(pattern.add(4), 0x90);
-	patch(pattern.add(5), 0xE9);
-}
+	patch(pattern.add(4), [0x90, 0xE9]);
