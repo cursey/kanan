82dd435a08576a135992d2252350455b058fd8fe
Coziest <paraparasomnia@gmail.com>
2016-07-24 19:48:18 -0700

    Add files via upload

---
+++ scripts/ClientSideDevCat.js
@@ -0,0 +1,8 @@
+// Gives You DevCat Title To See Extra Information In Various Places (Rydian)
+
+var pattern = scan('08 00 66 8B 41 10 C3');
+
+if (pattern == NULL)
+	send('Failed to apply patch.');
+else
+	patch(pattern.add(3), [0xB8, 0x61, 0xEA]);
