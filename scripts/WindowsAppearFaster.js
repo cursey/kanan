aa2a7c8de54fa0765eee74d1043f8198be4d29a0
cursey <cursey@live.com>
2016-07-26 02:19:58 -0700

    Forgot to check if pattern2 was null.

--- scripts/WindowsAppearFaster.js
+++ scripts/WindowsAppearFaster.js
@@ -1,7 +1,7 @@
 var pattern1 = scan('74 6F E8 ?? ?? ?? ?? 85 C0 74 10');
 var pattern2 = scan('8B 4D 18 6A 00 51 8B 8E');
 
-if (pattern1.isNull()) {
+if (pattern1.isNull() || pattern2.isNull()) {
     msg("Failed to apply patch.");
 }
 else {
