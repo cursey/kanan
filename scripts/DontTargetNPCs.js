94007e5eca40ffacdc7f3ac193f5a00805651a1c
cursey <cursey@live.com>
2016-07-25 22:29:45 -0700

    Add DontTargetNPCs.js

---
+++ scripts/DontTargetNPCs.js
@@ -0,0 +1,13 @@
+// Don't target NPCs when holding ctrl.
+// In IDA string view -> unicode strings
+// Search for 'enemy > npc'.
+// Change push to push 'enemy' (we reuse the enemy str found nearby).
+
+var pattern = scan('68 ?? ?? ?? ?? EB 05 68 ?? ?? ?? ?? 8D 8D ?? ?? ?? ?? E8 ?? ?? ?? ?? 8B 86');
+
+if (pattern.isNull()) {
+    msg("Failed to apply patch.");
+}
+else {
+    Memory.copy(pattern.add(1), pattern.add(8), 4);
+}
