a324a0fe91b5c964fb4d0011aae536a15ad5f271
cursey <cursey@live.com>
2016-07-28 02:23:56 -0700

    Removed some code, still seems to work.

--- scripts/UseDataFolder.js
+++ scripts/UseDataFolder.js
@@ -9,10 +9,6 @@
 // output.  Because of the crazy amount of debug text, most of it is only
 // outputed when also ran in verbose mode (-d -v).
 
-// Normal SetLookUpOrder patch.
-var pattern = scan('8B 40 20 83 F8 03');
-patch(pattern, [0x31, 0xC0, 0x90]);
-
 // SetLookUpOrder is only called once shortly after mabi starts, so we call it
 // again telling it to load from the data folder.
 var SetLookUpOrderPtr = scan('55 8B EC 6A FF 68 ?? ?? ?? ?? 64 A1 ?? ?? ?? ?? 50 83 EC 0C 53 56 57 A1 ?? ?? ?? ?? 33 C5 50 8D 45 F4 64 A3 ?? ?? ?? ?? 8B F1 89 75 EC E8 ?? ?? ?? ?? 84 C0');
@@ -26,7 +22,6 @@ dmsg("Called CFileSystem::SetLookUpOrder");
 // Allocate space for the redirected filenames.
 var filenameMem = allocateMemory(4096);
 var utf16Filename = filenameMem;
-var ansiFilename = filenameMem.add(2048);
 
 // Helpers to automatically redirect filenames.
 function getNewUtf16Filename(filenamePtr) {
@@ -39,21 +34,6 @@ function getNewUtf16Filename(filenamePtr) {
         Memory.writeUtf16String(utf16Filename, newFilename);
 
         return utf16Filename;
-    }
-
-    return filenamePtr;
-}
-
-function getNewAnsiFilename(filenamePtr) {
-    var filename = Memory.readAnsiString(filenamePtr);
-
-    // We only redirect filenames that have \data\ in them.
-    if (filename.includes('\\data\\')) {
-        var newFilename = path + filename.substr(filename.indexOf('\\data\\'));
-
-        Memory.writeAnsiString(ansiFilename, newFilename);
-
-        return ansiFilename;
     }
 
     return filenamePtr;
@@ -75,20 +55,8 @@ var onGetFileAttributesW = {
     }
 };
 
-var onGetFileAttributesA = {
-    onEnter(args) {
-        args[0] = getNewAnsiFilename(args[0]);
-
-        if (verbose) {
-            dmsg("GetFileAttributesA: " + Memory.readAnsiString(args[0]));
-        }
-    }
-};
-
 Interceptor.attach(getProcAddress('kernel32.dll', 'GetFileAttributesW'), onGetFileAttributesW);
 Interceptor.attach(getProcAddress('kernelbase.dll', 'GetFileAttributesW'), onGetFileAttributesW);
-Interceptor.attach(getProcAddress('kernel32.dll', 'GetFileAttributesA'), onGetFileAttributesA);
-Interceptor.attach(getProcAddress('kernelbase.dll', 'GetFileAttributesA'), onGetFileAttributesA);
 
 //
 // GetFileAttributesEx
@@ -103,20 +71,8 @@ var onGetFileAttributesExW = {
     }
 };
 
-var onGetFileAttributesExA = {
-    onEnter(args) {
-        args[0] = getNewAnsiFilename(args[0]);
-
-        if (verbose) {
-            dmsg("GetFileAttributesExA: " + Memory.readAnsiString(args[0]));
-        }
-    }
-};
-
 Interceptor.attach(getProcAddress('kernel32.dll', 'GetFileAttributesExW'), onGetFileAttributesExW);
 Interceptor.attach(getProcAddress('kernelbase.dll', 'GetFileAttributesExW'), onGetFileAttributesExW);
-Interceptor.attach(getProcAddress('kernel32.dll', 'GetFileAttributesExA'), onGetFileAttributesExA);
-Interceptor.attach(getProcAddress('kernelbase.dll', 'GetFileAttributesExA'), onGetFileAttributesExA);
 
 //
 // GetFullPathName
@@ -131,20 +87,8 @@ var onGetFullPathNameW = {
     }
 };
 
-var onGetFullPathNameA = {
-    onEnter(args) {
-        args[0] = getNewAnsiFilename(args[0]);
-
-        if (verbose) {
-            dmsg("GetFullPathNameA: " + Memory.readAnsiString(args[0]));
-        }
-    }
-};
-
 Interceptor.attach(getProcAddress('kernel32.dll', 'GetFullPathNameW'), onGetFullPathNameW);
 Interceptor.attach(getProcAddress('kernelbase.dll', 'GetFullPathNameW'), onGetFullPathNameW);
-Interceptor.attach(getProcAddress('kernel32.dll', 'GetFullPathNameA'), onGetFullPathNameA);
-Interceptor.attach(getProcAddress('kernelbase.dll', 'GetFullPathNameA'), onGetFullPathNameA);
 
 //
 // CreateFile
@@ -159,20 +103,8 @@ var onCreateFileW = {
     }
 };
 
-var onCreateFileA = {
-    onEnter(args) {
-        args[0] = getNewAnsiFilename(args[0]);
-
-        if (verbose) {
-            dmsg("CreateFileA: " + Memory.readAnsiString(args[0]));
-        }
-    }
-};
-
 Interceptor.attach(getProcAddress('kernel32.dll', 'CreateFileW'), onCreateFileW);
 Interceptor.attach(getProcAddress('kernelbase.dll', 'CreateFileW'), onCreateFileW);
-Interceptor.attach(getProcAddress('kernel32.dll', 'CreateFileA'), onCreateFileA);
-Interceptor.attach(getProcAddress('kernelbase.dll', 'CreateFileA'), onCreateFileA);
 
 //
 // FindFirstFile
@@ -187,20 +119,8 @@ var onFindFirstFileW = {
     }
 };
 
-var onFindFirstFileA = {
-    onEnter(args) {
-        args[0] = getNewAnsiFilename(args[0]);
-
-        if (verbose) {
-            dmsg("FindFirstFileA: " + Memory.readAnsiString(args[0]));
-        }
-    }
-};
-
 Interceptor.attach(getProcAddress('kernel32.dll', 'FindFirstFileW'), onFindFirstFileW);
 Interceptor.attach(getProcAddress('kernelbase.dll', 'FindFirstFileW'), onFindFirstFileW);
-Interceptor.attach(getProcAddress('kernel32.dll', 'FindFirstFileA'), onFindFirstFileA);
-Interceptor.attach(getProcAddress('kernelbase.dll', 'FindFirstFileA'), onFindFirstFileA);
 
 //
 // FindFirstFileEx
@@ -215,20 +135,8 @@ var onFindFirstFileExW = {
     }
 };
 
-var onFindFirstFileExA = {
-    onEnter(args) {
-        args[0] = getNewAnsiFilename(args[0]);
-
-        if (verbose) {
-            dmsg("FindFirstFileExA: " + Memory.readAnsiString(args[0]));
-        }
-    }
-};
-
 Interceptor.attach(getProcAddress('kernel32.dll', 'FindFirstFileExW'), onFindFirstFileExW);
 Interceptor.attach(getProcAddress('kernelbase.dll', 'FindFirstFileExW'), onFindFirstFileExW);
-Interceptor.attach(getProcAddress('kernel32.dll', 'FindFirstFileExA'), onFindFirstFileExA);
-Interceptor.attach(getProcAddress('kernelbase.dll', 'FindFirstFileExA'), onFindFirstFileExA);
 
 // The following don't modify their arguments because mabi does not directly
 // use the Nt* functions.  These are only intercepted for debugging.
@@ -287,7 +195,33 @@ if (debug && verbose) {
             var ObjectName = Memory.readUtf16String(ObjectNameBufferPtr);
             var filename = ObjectName;
 
-            msg("NtOpenFile: " + filename);
+            //var filename = Memory.readUtf16String(filenamePtr);
+
+            // We only redirect filenames that have \data\ in them.
+            if (filename.includes('\\data\\')) {
+                var newFilename = "\\??\\" + path + filename.substr(filename.indexOf('\\data\\'));
+
+                // Write the string.
+                Memory.writeUtf16String(utf16Filename.add(1024), newFilename);
+
+                // Change the UNICODE_STRING.
+                Memory.writeU16(ObjectNamePtr, newFilename.length * 2); // Length
+                Memory.writeU16(ObjectNamePtr.add(2), 1024); // MaximumLength
+                Memory.writePointer(ObjectNamePtr.add(4), utf16Filename.add(1024));
+
+                var ObjectAttributesPtr = args[2];
+                var ObjectNamePtr = Memory.readPointer(ObjectAttributesPtr.add(8));
+                var ObjectNameBufferPtr = Memory.readPointer(ObjectNamePtr.add(4));
+                var ObjectName = Memory.readUtf16String(ObjectNameBufferPtr);
+                var filename = ObjectName;
+
+                msg("NtOpenFile: " + filename);
+                msg("len: " + Memory.readU16(ObjectNamePtr));
+                msg("maxlen: " + Memory.readU16(ObjectNamePtr.add(2)));
+            }
+            else {
+                msg("NtOpenFile: " + filename);
+            }
         }
     });
 
