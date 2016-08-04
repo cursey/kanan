62f635a9ec8e4a0c738b9a5bc6ad400b7f8850e9
cursey <cursey@live.com>
2016-08-04 07:30:00 -0700

    Added FreeZoom.js

---
+++ scripts/FreeZoom.js
@@ -0,0 +1,7 @@
+// Find value that changes between 400.0,3000.0 as you zoom in and out.
+// Find what writes to this value. (fstp [esi+1Ch])
+// Change a jp and jnz above it to jmp.
+var pattern = scan('7A 05 D9 5D 08 EB 02 DD D8 D9 45 08 D9 45 F8');
+
+patch(pattern, 0xEB); // jp
+patch(pattern.add(22), 0xEB); // jnz
