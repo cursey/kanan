45e241e65cfb46b8eef316d0460052664486d646
cursey <cursey@live.com>
2016-08-04 08:49:45 -0700

    Add FarAltText.js

---
+++ scripts/FarAltText.js
@@ -0,0 +1,8 @@
+// Search for a static 3000.0 (there will be more than one so try until you
+// are certain you found the right one).
+// Find what accesses it
+// Look for an fld [<address>] that is called a ton
+// nop the jnz.
+var pattern = scan('0F 85 ?? ?? ?? ?? D9 EE D9 45 D4');
+
+patch(pattern, Array(6).fill(0x90));
