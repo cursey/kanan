1e92a0ff4fa8dc4ff9749785026b1eaf29da17be
cursey <cursey@live.com>
2016-08-07 16:45:14 -0700

    words.

--- scripts/NoChannelMoveDescription.js
+++ scripts/NoChannelMoveDescription.js
@@ -1,7 +1,7 @@
 // IDA unicode string view -> code.interface.msg.channel_move.desc
 // xrefs
 // Scroll down till you find a basic block that pushes the value 1 three times
-// and makes one call.  The function that is called should have the string
+// and makes one call.  The function that is called should reference the string
 // 'CMessageView' in it.
 // This is the call that creates the description message box.
 var firstPush = scan('6A 01 53 53 6A 01 53 6A 01 56 8D 4D DC');
