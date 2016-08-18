a6f13fb25a5b6a2acffb7fcdae1db9c41c9bd0e9
cursey <cursey@live.com>
2016-08-17 19:55:29 -0700

    Added show item price (Issue #40).

---
+++ scripts/ShowItemTradePrice.js
@@ -0,0 +1,23 @@
+// This one's really simple.  Turns out theres two obvious strings for us to
+// find!  Finding out what the strings were was easy enough.  Just open up
+// interface.english.txt and search for 'Shop Purchase Price:'.  This tells
+// you the name of the string we want to search in IDA for:
+//
+// 'window.pocket.tooltip.trade_pur_price'
+//
+// Of course you wont find that string in IDA without adding 'code.interface'
+// to it.  So the UNICODE string we search for is:
+//
+// 'code.interface.window.pocket.tooltip.trade_pur_price'
+//
+// Xrefs should show only a single reference, and further down in graph view
+// you should see:
+//
+// 'code.interface.window.pocket.tooltip.trade_sell_price'
+//
+// referenced as well.  Simply nop the jnz before the basic block you find the
+// first string in.
+
+var pattern = scan('0F 85 ?? ?? ?? ?? 68 ?? ?? ?? ?? 8D 4D F0 E8 ?? ?? ?? ?? 68 ?? ?? ?? ?? 8D 4D D8');
+
+patch(pattern, Array(6).fill(0x90));
