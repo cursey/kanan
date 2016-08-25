71463fa5deee9741b538b5db15804fc1bb003b4e
cursey <cursey@live.com>
2016-08-24 18:08:06 -0700

    Added ChatAllowSameMsg.js (requested in issue #41).

---
+++ scripts/ChatAllowSameMsg.js
@@ -0,0 +1,9 @@
+// Super simple, just open ida unicode string view and search for
+// code.interface.window.main_chat.msg_omit_same
+// xrefs
+// nop the two jnz's that jump to the basic block you find the reference in.
+var jnz1 = scan('0F 85 ? ? ? ? 33 FF C6 45 F3 01');
+var jnz2 = scan('0F 85 ? ? ? ? 33 FF 80 7D 10 00');
+
+patch(jnz1, Array(6).fill(0x90));
+patch(jnz2, Array(6).fill(0x90));
