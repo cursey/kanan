87b73073723980fb8d25ed1b8171881954e44f35
cursey <cursey@live.com>
2016-08-25 01:02:17 -0700

    Added ColorAltText.js (requested in issue #40).

---
+++ scripts/ColorAltText.js
@@ -0,0 +1,18 @@
+// Changes the color of the text you see when holding down alt.
+//
+// By default we change it to white.
+//
+// Find the function that generates the alt text (see ShowCombatPower.js).
+// The function that calls that text generating function also sets the the color
+// that the text will be, so xrefs to find the calling function.
+// Near the top you'll see some hex values being moved into a structure, they
+// look like FF99CC00h 3099CC00h FF3366FFh etc.
+// The one we want is the last one, FF3366FFh, to make it white just set it to
+// FFFFFFFFh. That will make everyone elses alt text white. To make our own alt
+// text white, change FF99CC00h if you want (by default we keep it as it is).
+//
+// I think the first part of the color code is alpha.
+
+var movColor = scan('C7 46 ? ? ? ? ? C7 46 ? ? ? ? ? 68');
+
+patch(movColor.add(3), [0xFF, 0xFF, 0xFF, 0xFF]);
