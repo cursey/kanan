569a1dce24649438418778cec6c4a908a4e67efd
poshwosh <vizimuley1@gmail.com>
2016-08-31 21:59:56 +0100

    RemoveDungeonFog.js
    This script removes the fog in dungeons.
    
    I am unsure who is the creator of this script, so I don't wish to take credit.
    
    I have found this patch in a certain script list of mod_sharker that isn't very well known/public. I've tested it and it worked.

---
+++ scripts/RemoveDungeonFog.js
@@ -0,0 +1,4 @@
+// Remove the fog in minimap while in dungeons. (Credit: ???)
+
+var pattern = scan('0F B6 41 05 C1 E8 02 83 E0 01 C3');
+patch(pattern.add(0), [0x33, 0xC0, 0xC3, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90]);
