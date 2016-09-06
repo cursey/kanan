756fd9e195dca426ba2fc4629b19ea2cfabc201d
cursey <cursey@live.com>
2016-09-06 03:49:43 -0700

    Hopefully fixed DisableNagle.js.  Please test!

--- scripts/DisableNagle.js
+++ scripts/DisableNagle.js
@@ -8,8 +8,33 @@ var WSAGetLastError = new NativeFunction(getProcAddress('Ws2_32.dll', 'WSAGetLas
 var IPPROTO_TCP = 6;
 var TCP_NODELAY = 0x0001;
 
+// This is actually not the call to socket(), but the call to the wrapper 
+// function Mabi's packer has created to hide the import. Intercepting this just
+// as if it was socket() is fine, but I got NGS kicked when intercepting the
+// actual socket() function (could have been a fluke I didn't do further 
+// testing).
+var socketCall = scan('E8 ? ? ? ? 8B F0 83 C4 0C 83 FE FF');
+var socketOffset = Memory.readS32(socketCall.add(1));
+var socketAddress = socketCall.add(5).toInt32() + socketOffset;
+
+Interceptor.attach(ptr(socketAddress), {
+    onLeave(retval) {
+        // retval will be the result of the call to socket().
+        var socket = retval.toInt32();
+        var nodelay = allocateMemory(4);
+
+        Memory.writeU32(nodelay, 1);
+
+        dmsg("Socket: " + socket);
+        dmsg("Setting TCP_NODELAY: " + setsockopt(socket, IPPROTO_TCP, TCP_NODELAY, nodelay, 4)); 
+        dmsg("WSAGetLastError: " + WSAGetLastError());
+
+        freeMemory(nodelay, 4);
+    }
+})
+
 // Calls socket().
-var createSocketPtr = scan('55 8B EC 83 EC 08 56 57 8B 7D 0C 8B CF');
+/*var createSocketPtr = scan('55 8B EC 83 EC 08 56 57 8B 7D 0C 8B CF');
 
 Interceptor.attach(createSocketPtr, {
     onEnter(args) {
@@ -29,3 +54,4 @@ Interceptor.attach(createSocketPtr, {
         freeMemory(nodelay, 4);
     }
 });
+*/
