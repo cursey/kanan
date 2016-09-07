06b4dd42c93f0b64c0a69b490be225b3d9f9a917
cursey <cursey@live.com>
2016-09-07 02:44:33 -0700

    Added ChatNoRateLimit.js because of #66 and #41

---
+++ scripts/ChatNoRateLimit.js
@@ -0,0 +1,8 @@
+// Easier than ChatAllowSameMsg.
+// IDA unicode string view search for:
+// code.interface.pli_chathelper.1
+// Will be in the same function as the string used for ChatAllowSameMsg.
+// Change the jbe that points to the basic block the reference is in to a jmp
+var thejbe = scan('76 B0 68 ? ? ? ? 8D 4D DC');
+
+patch(thejbe, 0xEB);
