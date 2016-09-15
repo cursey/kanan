e6693b45e85786dbb0033b5e8280207d1dce60c0
Zik <Poshwosh@users.noreply.github.com>
2016-09-15 18:24:57 +0100

    HotkeyAnyItem.js Fix for r235
    Mabinogi NA r235 broke this script, this is a fix.

--- scripts/HotkeyAnyItem.js
+++ scripts/HotkeyAnyItem.js
@@ -1,11 +1,12 @@
 // Allows not just potions to be hotkeyed, but ANY sort of usable item. (Created by ???, fixed by Poshwosh)
 
-// Original mod_sharker script for future reference, I couldn't "pinpoint" this pattern accurately so it will very likely break eventually:
+// Original mod_sharker script for future reference (this mod is hard to "pinpoint" and has been changed throughout the years so it will very likely break eventually again):
 // Item Hotkey [MOD]
 // [ItemHotkey]
 // Search1=84 C0 74 04 B0 01 EB 02 32 C0 C2 04 00
 // Replace1=84 C0 74 04 B0 01 EB 02 B0 01 C2 04 00
-// Value found on r234 NA = 02 E8 EC 72 5D FF >84 C0 74< 06 >B0 01< 5D C2 04 00 >32 C0< 5D C2 04 00 CC
+// Value found on r235 NA = >84 C0 74< 06 >B0 01< 5D C2 04 00 >32 C0< 5D C2 04 00 CC
+// There are multiple results, luckily the first result is the right one.
 
-var pattern = scan('EC ?? ?? ?? 84 C0 74 ?? B0 01 ?? ?? ?? ?? 32 C0');
-patch(pattern.add(14), [0xB0, 0x01]);
+var pattern = scan('84 C0 74 06 B0 01 5D C2 04 00 32 C0 5D C2 04 00 CC');
+patch(pattern.add(10), [0xB0, 0x01]);
