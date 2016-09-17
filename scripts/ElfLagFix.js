80682bc03d8af9a6250749bec79847324e55c32d
Zik <Poshwosh@users.noreply.github.com>
2016-09-17 16:56:17 +0100

    Updated description

--- scripts/ElfLagFix.js
+++ scripts/ElfLagFix.js
@@ -1,4 +1,5 @@
-// Elf Lag Fix
+// Description:
+// Fixes the issue that causes Elf characters with high latency to freeze in place while using the skill Ranged Attack. (created by Blade3575)
 
 var pattern = scan('CC 55 8B EC 56 57 8B F9 8B 07 8B 50 04 FF D2 8B C8 E8');
 
