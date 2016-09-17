f2c8695cf536b6e37db7d4dafab2e754cd05c2a8
Zik <Poshwosh@users.noreply.github.com>
2016-09-17 17:02:06 +0100

    Updated description

--- scripts/FighterNoForce.js
+++ scripts/FighterNoForce.js
@@ -1,5 +1,7 @@
-// No Persistent Fighter Chain Popup (Rydian)
+// Description: 
+// While in hide interface mode, using chain combo fighter skills will turn the interface back on. This disables this trigger, persisting the interface to stay hidden. (created by Rydian)
 
-var pattern = scan('83 79 44 01 75 09 6A 00 6A 00 E8');
-
-patch(pattern.add(4), 0xEB);
+var secondchain = scan('83 79 44 01 75 09 6A 00 6A 00 E8');
+patch(secondchain.add(4), 0xEB);
+var thirdchain = scan('83 79 44 01 75 09 6A 00 6A 00 E8');
+patch(thirdchain.add(4), 0xEB);
