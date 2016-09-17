98a3a3a62b4d5cd5ad19853041800de0ec41cdb5
Zik <Poshwosh@users.noreply.github.com>
2016-09-17 16:45:46 +0100

    Updated description

--- scripts/AlwaysTransCollectMode.js
+++ scripts/AlwaysTransCollectMode.js
@@ -1,4 +1,5 @@
-// Transformation Mastery Collect Mode Always Enabled (Rydian)
+// Description:
+// Automatically enable Transformation Mastery's Collect Mode and persist it through channel change and log out. (created by Rydian)
 
 var pattern = scan('38 5F 78 0F 84 C9 02 00 00 8B CE');
 
