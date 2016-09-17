e3956d608c8a2093032e8a36c6ccb8f1ed9a2da3
Zik <Poshwosh@users.noreply.github.com>
2016-09-17 17:01:14 +0100

    Updated description

--- scripts/UncapAutoProduction.js
+++ scripts/UncapAutoProduction.js
@@ -1,4 +1,5 @@
-// Removes the default cap when producing items through automatic production with the skills Refining, Weaving, Potion Making, Handicraft, Carpentry, Engineering and Magic Craft. (Created by Step29, converted by C0ZIEST)
+// Description: 
+// Removes the default cap when producing items through automatic production with the skills Refining, Weaving, Potion Making, Handicraft, Carpentry, Engineering and Magic Craft. (created by Step29)
 
 var pattern1 = scan('66 39 86 ?? ?? ?? ?? 76 07 66');
 patch(pattern1.add(7), 0xEB);
