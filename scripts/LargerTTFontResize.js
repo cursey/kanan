4ad77ff5001f58aa12b2c41e8f153d116c608e2e
Zik <Poshwosh@users.noreply.github.com>
2016-09-17 17:19:34 +0100

    Updated description

--- scripts/LargerTTFontResize.js
+++ scripts/LargerTTFontResize.js
@@ -1,8 +1,6 @@
-// Larger TrueType Font size. Not compatible with bitmap font, if enabled.
-// Notes:
-//  - If you're using bots that search text images, this will break them.
-//  - Dev: Cannot increase font size any higher without causing crashes.
-//  - There's also some text overflow/overlap on some windows to live with.
+// Description: 
+// Increase the font size close to 14px in size. (created by ???)
+// Note that it is incompatible with BitmapFont.js, it will break image searching bots, and there's also some text overflow/overlap on some windows to live with.
 
 var pattern = scan('c7 86 a4 00 00 00 0b 00 00 00 c7');
 
