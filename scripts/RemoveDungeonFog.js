7b8a3546ae5a89b50dd5b29e94f7931664f3ed0b
Zik <Poshwosh@users.noreply.github.com>
2016-09-17 17:07:18 +0100

    Updated description

--- scripts/RemoveDungeonFog.js
+++ scripts/RemoveDungeonFog.js
@@ -1,4 +1,5 @@
-// Remove the fog in minimap while in dungeons. (Credit: ???)
+// Description:
+// Remove the fog on the map while in dungeons, automatically unveiling them. (created by ???)
 
 var pattern = scan('0F B6 41 05 C1 E8 02 83 E0 01 C3');
 patch(pattern.add(0), [0x33, 0xC0, 0xC3, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90]);
