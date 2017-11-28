33f9370acec7186b1c08c3d171de6488846535cf
Lishid <lishid@gmail.com>
2017-11-28 05:26:00 -0500

    Update NoRewardWindow.js

--- scripts/NoRewardWindow.js
+++ scripts/NoRewardWindow.js
@@ -1,6 +1,6 @@
 // Description:
 // Removes the "Obtained X item" reward window that shows up when acquiring items, stats or exp. (created by Licat)
 
-var pattern = scan('39 5D 14 0F 85 ?? ?? ?? ?? 8D 4D EC');
+var pattern = scan('3B D6 0F 85 ?? ?? ?? ?? 33 DB');
 
-patch(pattern, [0x90, 0x39, 0xDB, 0x0f, 0x84]);
+patch(pattern, [0x39, 0xDB, 0x0f, 0x84]);
