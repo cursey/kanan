6549da6e2fee1eca0423ef2fe9a16d7d117fa247
Lishid <lishid@gmail.com>
2017-12-13 17:10:03 -0500

    Create ShowFPS.js

---
+++ scripts/ShowFPS.js
@@ -0,0 +1,11 @@
+// Description:
+// Show clean FPS counter. (created by Licat)
+
+var pattern = scan('8B F8 8B CE 89 BD DC F7 FF FF E8 ?? ?? ?? ?? 8B C8 E8 ?? ?? ?? ?? 8B 10 8B C8 8B 42 04 FF D0 84 C0 0F 84');
+var fpsStr = scan('25 00 64 00 20 00 66 00 72 00 61 00 6D 00 65 00 73 00 2F 00 73 00 65 00 63 00 2C 00');
+
+patch(pattern.add(33), Array(6).fill(0x90));
+patch(pattern.add(130), [0xE9, 0xB2, 0x00, 0x00, 0x00, 0x90]);
+patch(pattern.add(461), [0x0A, 0x00, 0x00, 0x00]);
+patch(pattern.add(521), [0x39, 0xC0]);
+patch(fpsStr, [0x46, 0x00, 0x50, 0x00, 0x53, 0x00, 0x3A, 0x00, 0x20, 0x00, 0x25, 0x00, 0x64, 0x00, 0x00, 0x00]);
